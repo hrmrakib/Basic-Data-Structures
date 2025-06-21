8d6ac87a479a936c58c909613c2c87307971e4a3
hrmrakib <hrmrakibs@gmail.com>
2025-06-21 11:30:50 +0600

    Vector String Input and Output

---
+++ mod-2_STL_Vector/Vector_input_and_output.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // 1: if have a size
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    // 2: without size (dynamic vector)
+    int x;
+    cin >> x;
+    vector<int>v2;
+    for(int i = 0; i < x; i++) {
+        int a;
+        cin >> a;
+        v2.push_back(a);
+    }
+
+    return 0;
+}
---
+++ mod-2_STL_Vector/Vector_of_String.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // int n;
+    // cin >> n;
+    // cin.ignore();
+    // vector<string> v(n);
+
+    // for(int i = 0; i < n; i++){
+    //     getline(cin, v[i]);
+    // }
+    // for(string s : v){
+    //     cout << s << endl;
+    // }
+
+    vector<int> v={1,2,3,4};
+v.resize(2);
+v.resize(4);
+
+for (int x: v){
+    cout << x << " ";
+}
+    return 0;
+}
