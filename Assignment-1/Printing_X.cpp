93d43410602e7d62d8f4082ee8ea3b2a0da12004
hrmrakib <hrmrakibs@gmail.com>
2025-06-28 14:40:35 +0600

    ..

--- Assignment-1/Printing_X.cpp
+++ Assignment-1/Printing_X.cpp
@@ -10,7 +10,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(i == j) {
-                cout << " X ";
+                
             }
         }
         cout << endl;
