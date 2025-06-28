b96bf019e57b2853384fe5da0acb202db5f16eb5
hrmrakib <hrmrakibs@gmail.com>
2025-06-29 00:03:03 +0600

    Solved: Printing X

--- Assignment-1/Printing_X.cpp
+++ Assignment-1/Printing_X.cpp
@@ -9,51 +9,32 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            if(i == j) {
-                
+            if(i < mid) {
+                if(i == j) {
+                    cout << "\\";
+                }else if(j == n - i - 1) {
+                    cout << "/";
+                }else {
+                    cout << " ";
+                }
+            }else if(i == mid) {
+                if(j == mid) {
+                    cout << "X";
+                }else{
+                    cout << " ";
+                }
+            }else{
+                if(j == n - i - 1){
+                    cout << "/";
+                }else if(j == i){
+                    cout << "\\";
+                }else{
+                    cout << " ";
+                }
             }
         }
         cout << endl;
     }
+
     return 0;
 }
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main() {
-//     int n;
-//     cin >> n;
-
-//     int mid = n / 2;
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < n; j++) {
-//             if (i < mid) {
-//                 // Upper half
-//                 if (j == i)
-//                     cout << "\\";
-//                 else if (j == n - i - 1)
-//                     cout << "/";
-//                 else
-//                     cout << " ";
-//             } else if (i == mid) {
-//                 // Middle line
-//                 if (j == mid)
-//                     cout << "X";
-//                 else
-//                     cout << " ";
-//             } else {
-//                 // Lower half
-//                 if (j == n - i - 1)
-//                     cout << "/";
-//                 else if (j == i)
-//                     cout << "\\";
-//                 else
-//                     cout << " ";
-//             }
-//         }
-//         cout << endl;
-//     }
-
-//     return 0;
-// }
