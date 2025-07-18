8220b7420ded3c39fd81846dc6af171606938bd9
hrmrakib <hrmrakibs@gmail.com>
2025-07-18 22:18:43 +0600

    doubly linked list inserted any position

---
+++ mod-9_Doubly_Linked_List/4_Insert_at_tail.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public: 
+        int val;
+        Node *next;
+        Node *prev;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head) {
+    Node* temp = head;
+
+    while(temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node* tail) {
+    Node* temp = tail;
+
+    while(temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int val) {
+    Node* newnode = new Node(val);
+
+    if(head == NULL) {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    newnode->prev = tail;
+    tail->next = newnode;
+    newnode = tail;
+}
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_tail(head, tail, 100);
+
+    print_forward(head);
+    print_backward(tail);
+
+    return 0;
+}
