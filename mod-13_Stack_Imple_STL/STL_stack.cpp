cd680bdac96e579f3f01f7ab2d3ccd28c32c9b57
hrmrakib <hrmrakibs@gmail.com>
2025-08-11 18:22:48 +0600

    temp commmit

--- mod-13_Stack_Imple_STL/STL_stack.cpp
+++ mod-13_Stack_Imple_STL/STL_stack.cpp
@@ -4,19 +4,18 @@ using namespace std;
 int main()
 {
     stack<int> s;
-    
+
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
         s.push(x);
     }
 
-
-    while(!s.empty())
+    while (!s.empty())
     {
         cout << s.top() << endl;
         s.pop();
---
+++ mod-14_Queue_Imple_STL/Implement_Queue_using_Singly_linked list.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
