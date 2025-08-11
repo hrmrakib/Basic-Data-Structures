3c7816165f3f27a64a6bba7376b0ffbf39d77e63
hrmrakib <hrmrakibs@gmail.com>
2025-08-11 19:07:40 +0600

    last commit from office

--- code.cpp
+++ code.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int paildrome(int x)
+{
+    int n = x;
+    int rev = 0;
+    while (x)
+    {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+    return rev == n;
+}
+
 int main()
 {
-    list<string> l;
-    while (true)
-    {
-        string web;
-        cin >> web;
-        if (web == "end")
-        {
-            break;
-        }
-        l.push_back(web);
-    }
-
-    for (auto it = l.begin(); it != l.end(); ++it)
-    {
-        cout << *it << " ";
-    }
+    int x = paildrome(123);
+    cout << "OK" << x << endl;
     return 0;
 }
